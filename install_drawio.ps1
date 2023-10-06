f00c45e3acdb760a202c55e539f17148605e8786
Peter <peter.hsu@concords.com.tw>
2023-10-06 10:16:38 +0800

    ,

--- install_drawio.ps1
+++ install_drawio.ps1
@@ -1,24 +1,63 @@
 function Install {
+    # (1) 參數設定 - - - - - - - - - - - - (1) 參數設定 - - - - - - - - - - - - (1) 參數設定 - - - - - - - - - - - -
+    $DownloadUrl = "https://github.com/jgraph/drawio-desktop/releases/download/v22.0.2/draw.io-22.0.2-windows-installer.exe"
+    $KiloathDir = Join-Path $HOME "KiloathApp"
+    $Directory = Join-Path $KiloathDir "DrawIO"
+    $Target = Join-Path $Directory "draw.io-22.0.2-windows-installer.exe"
+    $BinDir = "$Directory"
+    $BinExe = "$BinDir\draw.io.exe"
+    $AppName = "DrawIO"
+    # (2) 需要7z來解壓縮 - - - - - - - - - (2) 需要7z來解壓縮 - - - - - - - - - (2) 需要7z來解壓縮 - - - - - - - - -
+    # <#
     if (-not (Get-Command "7zr.exe" -ErrorAction SilentlyContinue)) {
         (Invoke-WebRequest "https://raw.githubusercontent.com/kiloath/Installer/main/install_7zr.ps1" -UseBasicParsing).Content | Invoke-Expression
     }
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $DownloadUrl = "https://github.com/jgraph/drawio-desktop/releases/download/v21.6.8/draw.io-21.6.8-windows-installer.exe"
-    $KiloathDir = Join-Path $HOME "KiloathApp"
-    $Directory = Join-Path $KiloathDir "DrawIO"
-    $Target = Join-Path $Directory "draw.io-21.6.8-windows-installer.exe"
-    New-Item $Directory -Force -ItemType Directory | Out-Null
-    Invoke-WebRequest $DownloadUrl -OutFile $Target -UseBasicParsing
-    $BinDir = $Directory
-    Start-Process -FilePath "7zr.exe" -ArgumentList "x $Target -o""$BinDir"" -y" | Out-Null
+    #>
+    # (3) 是否已下載 - - - - - - - - - - - (3) 是否已下載 - - - - - - - - - - - (3) 是否已下載 - - - - - - - - - - -
+    if(($file = Get-Item $Target -ErrorAction SilentlyContinue) -And ($file.Length -eq 104102712)) {
+        Write-Host "你已下載最新版"
+    }
+    else {
+        New-Item $Directory -Force -ItemType Directory | Out-Null
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        Invoke-WebRequest $DownloadUrl -OutFile $Target -UseBasicParsing        
+    }
+    # (4) 解壓縮 - - - - - - - - - - - - - (4) 解壓縮 - - - - - - - - - - - - - (4) 解壓縮 - - - - - - - - - - - - -
+    Start-Process -FilePath "7zr.exe" -ArgumentList "x $Target -o""$Directory"" -y" | Out-Null
+    # Expand-Archive -Path $Target -DestinationPath $Directory -Force
+    # (5) 設定 Path- - - - - - - - - - - - (5) 設定 Path- - - - - - - - - - - - (5) 設定 Path- - - - - - - - - - - -
+    <#
+    $regexInstallPath = [regex]::Escape($BinDir)
+    if (-Not ($env:Path -Match "$regexInstallPath")) {
+        [Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", "User").TrimEnd(";") + ";" + $BinDir, "User")
+        $env:Path = $env:Path.TrimEnd(";") + ";" + $BinDir
+    }
+    #>
+    # (6) 設定右鍵功能 - - - - - - - - - - (6) 設定右鍵功能 - - - - - - - - - - (6) 設定右鍵功能 - - - - - - - - - -
+    # 6.1 檔案
+    # <#
+    New-Item -Path HKCU:\SOFTWARE\Classes\*\shell\$AppName -value "Kiloath $AppName" -Force | Out-Null
+    New-ItemProperty -LiteralPath HKCU:\SOFTWARE\Classes\*\shell\$AppName -Name "Icon" -Value "$BinExe" -Force | Out-Null
+    New-Item -Path HKCU:\SOFTWARE\Classes\*\shell\$AppName\command -value """$BinExe"" ""%1""" -Force | Out-Null
+    #>
+    # 6.2 目錄
+    <#
+    New-Item -Path HKCU:\SOFTWARE\Classes\Directory\shell\$AppName -value "Kiloath $AppName" -Force | Out-Null
+    New-ItemProperty -LiteralPath HKCU:\SOFTWARE\Classes\Directory\shell\$AppName -Name "Icon" -Value "$BinExe" -Force | Out-Null
+    New-Item -Path HKCU:\SOFTWARE\Classes\Directory\shell\$AppName\command -value """$BinExe"" ""%V""" -Force | Out-Null
+    #>
+    # (7) 捷徑 - - - - - - - - - - - - - - (7) 捷徑 - - - - - - - - - - - - - - (7) 捷徑 - - - - - - - - - - - - - -
+    # <#
     $WshShell = New-Object -comObject WScript.Shell
-    $Shortcut = $WshShell.CreateShortcut("$KiloathDir\draw.io.lnk")
-    $Shortcut.TargetPath = "$BinDir\draw.io.exe"
+    $Shortcut = $WshShell.CreateShortcut("$KiloathDir\$AppName.lnk")
+    $Shortcut.TargetPath = $BinExe
     $Shortcut.Save()
     $Shortcut = $WshShell.CreateShortcut("$([Environment]::GetFolderPath('Desktop'))\KiloathApp.lnk")
     $Shortcut.TargetPath = Join-Path $HOME "KiloathApp"
     $Shortcut.Save()
-
+    #>
 }
 
+Write-Host "--- 安裝 DrawIO ---"
 Install
+Write-Host "--- 完成 DrawIO ---"
