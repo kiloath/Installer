8325bbd9ee4dcf1a46360944292dd6a87bdbeede
Kiloath <Kiloath@outlook.com>
2023-11-15 15:17:35 +0800

    ,

--- install_git_setup.ps1
+++ install_git_setup.ps1
@@ -7,7 +7,6 @@ function Setup {
     git config --global mergetool.winmerge.prompt false
     git config --global mergetool.keepBackup false
     git config --global mergetool.winmerge trustExitCode false
-    C:\Users\WDAGUtilityAccount\AppData\Local\Programs\WinMerge\WinMergeU.exe                        /e /ub  /fr  /wl  /wm       /dl %bname     /dm %tname      /dr %yname  %base       %theirs         %mine /o %merged /ar
     cmd /c "git config --global mergetool.winmerge.cmd `"\`"$HOME\KiloathApp\winmerge\WinMergeU.exe\`" //ub //fr //wl //wm //ar //dl \`"基底\`" //dm MERGE_HEAD //dr HEAD \`"`$BASE\`" \`"`$REMOTE\`" \`"`$LOCAL\`" //o \`"`$MERGED\`"`""
     cmd /c "git config --global core.editor `"\`"$HOME\KiloathApp\notepad++\notepad++.exe\`" -multiInst -notabbar -nosession -noPlugin`""
 }
