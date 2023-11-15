83222b374ef2fe00e3ef9a4b72870e6afc2c2b26
Kiloath <Kiloath@outlook.com>
2023-11-15 11:35:17 +0800

    ,

--- bundle_git.ps1
+++ bundle_git.ps1
@@ -1,2 +1,6 @@
-Write-Host "= (01)/01 進度 git ="
+Write-Host "= (01)/02 進度 git,notepad++,winmerge ="
 (Invoke-WebRequest "https://raw.githubusercontent.com/kiloath/Installer/main/install_git.ps1" -UseBasicParsing).Content | Invoke-Expression
+Write-Host "= (02)/02 進度 notepad++,winmerge ="
+(Invoke-WebRequest "https://raw.githubusercontent.com/kiloath/Installer/main/install_notepad++.ps1" -UseBasicParsing).Content | Invoke-Expression
+Write-Host "= (03)/03 進度 winmerge ="
+(Invoke-WebRequest "https://raw.githubusercontent.com/kiloath/Installer/main/install_winmerge.ps1" -UseBasicParsing).Content | Invoke-Expression
