5dd4dd0a9c719b99fa0ed67ca817a2242195af30
Kiloath <Kiloath@outlook.com>
2023-11-15 14:27:29 +0800

    ,

--- bundle_git.ps1
+++ bundle_git.ps1
@@ -1,5 +1,3 @@
-Write-Host "= (01)/04 進度 powershell7,git,notepad++,winmerge,git_setup ="
-(Invoke-WebRequest "https://raw.githubusercontent.com/kiloath/Installer/main/install_powershell7.ps1" -UseBasicParsing).Content | Invoke-Expression
 Write-Host "= (01)/04 進度 git,notepad++,winmerge,git_setup ="
 (Invoke-WebRequest "https://raw.githubusercontent.com/kiloath/Installer/main/install_git.ps1" -UseBasicParsing).Content | Invoke-Expression
 Write-Host "= (02)/04 進度 notepad++,winmerge,git_setup ="
