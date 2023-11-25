909c11ed789cbdc62f43fc63aef147bf7a059c1d
Kiloath <Kiloath@outlook.com>
2023-11-25 23:32:46 +0800

    ,

--- install_vscode_setup.ps1
+++ install_vscode_setup.ps1
@@ -1,6 +1,6 @@
 $setting_argv_Path = "$HOME\.vscode\argv.json"
 if(!(Test-Path $setting_argv_Path)) {
-    New-Item $setting_argv_Path -Force -ItemType File | Out-Null
+    New-Item $setting_argv_Path -Force -ItemType "file" | Out-Null
 }
 $settings = Get-Content $setting_argv_Path -raw | ConvertFrom-Json
 $settings.profiles.defaults | add-member -Name "locale" -Value "zh-tw" -MemberType NoteProperty -Force
