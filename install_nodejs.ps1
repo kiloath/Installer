2ca08159c1fd89a117e89dbd4ea6b1223e502c85
Peter <peter.hsu@concords.com.tw>
2024-02-26 10:07:17 +0800

    ,

--- install_nodejs.ps1
+++ install_nodejs.ps1
@@ -1,17 +1,10 @@
 function Install {
     # (1) 參數設定 - - - - - - - - - - - - (1) 參數設定 - - - - - - - - - - - - (1) 參數設定 - - - - - - - - - - - -
-    $DownloadUrl = "https://nodejs.org/dist/v20.10.0/node-v20.10.0-win-x64.zip"
+    $DownloadUrl = "https://nodejs.org/dist/v20.11.1/node-v20.11.1-win-x64.zip"
     $KiloathDir = Join-Path $HOME "KiloathApp"
     $Directory = Join-Path $KiloathDir "nodejs"
-    $Target = Join-Path $Directory "node-v20.10.0-win-x64.zip"
+    $Target = Join-Path $Directory "node-v20.11.1-win-x64.zip"
     $BinDir = "$Directory\nodejs"
-    # $BinExe = "$BinDir\dotnet.exe"
-    # (2) 需要7z來解壓縮 - - - - - - - - - (2) 需要7z來解壓縮 - - - - - - - - - (2) 需要7z來解壓縮 - - - - - - - - -
-    <#
-    if (-not (Get-Command "7zr.exe" -ErrorAction SilentlyContinue)) {
-        (Invoke-WebRequest "https://raw.githubusercontent.com/kiloath/Installer/main/install_7zr.ps1" -UseBasicParsing).Content | Invoke-Expression
-    }
-    #>
     # (3) 是否已下載 - - - - - - - - - - - (3) 是否已下載 - - - - - - - - - - - (3) 是否已下載 - - - - - - - - - - -
     if(Get-Item $Target -ErrorAction SilentlyContinue) {
         Write-Host "你已下載最新版"
@@ -23,47 +16,20 @@ function Install {
         Invoke-WebRequest $DownloadUrl -OutFile $Target -UseBasicParsing        
     }
     # (4) 解壓縮 - - - - - - - - - - - - - (4) 解壓縮 - - - - - - - - - - - - - (4) 解壓縮 - - - - - - - - - - - - -
-    # Start-Process -FilePath "7zr.exe" -ArgumentList "x $Target -o""$Directory"" -y" | Out-Null
     Expand-Archive -Path $Target -DestinationPath $Directory -Force
     If (Test-Path -Path $BinDir) {
          Remove-Item -Path $BinDir -Recurse -Force | Out-Null
     }
-    If (Test-Path -Path "$Directory\node-v20.10.0-win-x64") {
-         Rename-Item -Path "$Directory\node-v20.10.0-win-x64" -NewName nodejs -Force | Out-Null
+    If (Test-Path -Path "$Directory\node-v20.11.1-win-x64") {
+         Rename-Item -Path "$Directory\node-v20.11.1-win-x64" -NewName nodejs -Force | Out-Null
     }
     # (5) 設定 Path- - - - - - - - - - - - (5) 設定 Path- - - - - - - - - - - - (5) 設定 Path- - - - - - - - - - - -
-    # <#
     $regexInstallPath = [regex]::Escape($BinDir)
     if (-Not ($env:Path -Match "$regexInstallPath")) {
         [Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", "User").TrimEnd(";") + ";" + $BinDir, "User")
         $env:Path = $env:Path.TrimEnd(";") + ";" + $BinDir
     }
-    #>
-    # (6) 設定右鍵功能 - - - - - - - - - - (6) 設定右鍵功能 - - - - - - - - - - (6) 設定右鍵功能 - - - - - - - - - -
-    # 6.1 檔案
-    <#
-    New-Item -Path HKCU:\SOFTWARE\Classes\*\shell\$AppName -value "Kiloath $AppName" -Force | Out-Null
-    New-ItemProperty -LiteralPath HKCU:\SOFTWARE\Classes\*\shell\$AppName -Name "Icon" -Value "$BinExe" -Force | Out-Null
-    New-Item -Path HKCU:\SOFTWARE\Classes\*\shell\$AppName\command -value """$BinExe"" ""%1""" -Force | Out-Null
-    #>
-    # 6.2 目錄
-    <#
-    New-Item -Path HKCU:\SOFTWARE\Classes\Directory\shell\$AppName -value "Kiloath $AppName" -Force | Out-Null
-    New-ItemProperty -LiteralPath HKCU:\SOFTWARE\Classes\Directory\shell\$AppName -Name "Icon" -Value "$BinExe" -Force | Out-Null
-    New-Item -Path HKCU:\SOFTWARE\Classes\Directory\shell\$AppName\command -value """$KiloathDir\powershell\pwsh.exe"" -WorkingDirectory ""%V"" -NoLogo -NoExit -File ""$Directory\net8sdk.ps1""" -Force | Out-Null
-    #>
-    # (7) 捷徑 - - - - - - - - - - - - - - (7) 捷徑 - - - - - - - - - - - - - - (7) 捷徑 - - - - - - - - - - - - - -
-    <#
-    $WshShell = New-Object -comObject WScript.Shell
-    $Shortcut = $WshShell.CreateShortcut("$KiloathDir\$AppName.lnk")
-    $Shortcut.TargetPath = $BinExe
-    $Shortcut.Save()
-    $Shortcut = $WshShell.CreateShortcut("$([Environment]::GetFolderPath('Desktop'))\KiloathApp.lnk")
-    $Shortcut.TargetPath = Join-Path $HOME "KiloathApp"
-    $Shortcut.Save()
-    #>
-}
-   
+}   
 
 Write-Host "--- 安裝 nodejs ---"
 Install
