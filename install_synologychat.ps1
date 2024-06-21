826ca52911c11955d85d08ddf07b9ca34919c040
Peter <peter.hsu@concords.com.tw>
2024-06-21 09:14:57 +0800

    ,

--- install_synologychat.ps1
+++ install_synologychat.ps1
@@ -5,6 +5,9 @@ function Install {
     $KiloathDir = Join-Path $HOME "KiloathApp"
     $Directory = Join-Path $KiloathDir "Chat"
     $Target = Join-Path $Directory "Synology_Chat_Client-$latest_version.exe"
+    $BinDir = "$Directory"
+    $BinExe = "$BinDir\Synology Chat.exe"
+    $AppName = "Synology Chat"
 
     # (3) 是否已下載 - - - - - - - - - - - (3) 是否已下載 - - - - - - - - - - - (3) 是否已下載 - - - - - - - - - - -
     if(Get-Item $Target -ErrorAction SilentlyContinue) {
@@ -15,11 +18,18 @@ function Install {
         New-Item $Directory -Force -ItemType Directory | Out-Null
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
         Invoke-WebRequest $DownloadUrl -OutFile $Target -UseBasicParsing
-        &$Target /VERYSILENT /DIR=$Directory
     }
 
     # (4) 解壓縮 - - - - - - - - - - - - - (4) 解壓縮 - - - - - - - - - - - - - (4) 解壓縮 - - - - - - - - - - - - -
     Start-Process -FilePath "7zr.exe" -ArgumentList "x $Target -o""$Directory"" -y" | Out-Null
+     # (7) 捷徑 - - - - - - - - - - - - - - (7) 捷徑 - - - - - - - - - - - - - - (7) 捷徑 - - - - - - - - - - - - - -
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("$KiloathDir\$AppName.lnk")
+    $Shortcut.TargetPath = $BinExe
+    $Shortcut.Save()
+    $Shortcut = $WshShell.CreateShortcut("$([Environment]::GetFolderPath('Desktop'))\KiloathApp.lnk")
+    $Shortcut.TargetPath = Join-Path $HOME "KiloathApp"
+    $Shortcut.Save()
 }
 
 Write-Host "--- 安裝 Synology Chat ---"
