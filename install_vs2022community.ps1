56ec2666b6393bbc1a62064b20683988adf4d1a5
Kiloath <Kiloath@outlook.com>
2024-08-03 18:40:14 +0800

    ,

--- install_vs2022community.ps1
+++ install_vs2022community.ps1
@@ -15,7 +15,7 @@ function Install {
         Invoke-WebRequest $DownloadUrl -OutFile $Target -UseBasicParsing
         Start-Process -FilePath "$Target" -ArgumentList "--layout $Directory\VSlayout --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended --includeOptional --lang en-US --passive" -wait
     }
-    Start-Process -FilePath "$Directory\VSlayout\vs_setup.exe" -ArgumentList "--add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended --includeOptional --lang en-US --passive" -wait
+    Start-Process -FilePath "$Directory\VSlayout\vs_setup.exe" -ArgumentList "--noWeb --wait --passive" -wait
     $BinDir = "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin"
     $regexInstallPath = [regex]::Escape($BinDir)
     if (-Not ($env:Path -Match "$regexInstallPath")) {
