b75e80eb2338fb391b29b0ec5402d7e93621ba54
Peter <peter.hsu@concords.com.tw>
2024-09-27 16:08:34 +0800

    ,

--- install_krita.ps1
+++ install_krita.ps1
@@ -8,7 +8,7 @@ function Install {
     New-Item $Directory -Force -ItemType Directory | Out-Null
     Invoke-WebRequest $DownloadUrl -OutFile $Target -UseBasicParsing
     Expand-Archive -Path $Target -DestinationPath $Directory -Force
-    $BinDir = Join-Path $Directory $DownloadFile "bin"
+    $BinDir = Join-Path $Directory "krita-x64-$Version" "bin"
     $WshShell = New-Object -comObject WScript.Shell
     $Shortcut = $WshShell.CreateShortcut("$KiloathDir\krita.lnk")
     $Shortcut.TargetPath = "$BinDir\krita.exe"
