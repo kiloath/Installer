e1e4312cfddca987c58f403a02edb115ec6ef9dc
Peter <peter.hsu@concords.com.tw>
2024-09-27 16:06:25 +0800

    ,

--- install_krita.ps1
+++ install_krita.ps1
@@ -1,10 +1,10 @@
 function Install {
     $Version = "5.2.5"
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $DownloadUrl = "https://download.kde.org/stable/krita/$Version/krita-$Version.zip"
+    $DownloadUrl = "https://download.kde.org/stable/krita/$Version/krita-x64-$Version.zip"
     $KiloathDir = Join-Path $HOME "KiloathApp"
     $Directory = Join-Path $KiloathDir "Krita"
-    $Target = Join-Path $Directory "krita-$Version.zip"
+    $Target = Join-Path $Directory "krita-x64-$Version.zip"
     New-Item $Directory -Force -ItemType Directory | Out-Null
     Invoke-WebRequest $DownloadUrl -OutFile $Target -UseBasicParsing
     Expand-Archive -Path $Target -DestinationPath $Directory -Force
