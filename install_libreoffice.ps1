9e49d1860758614837915fa49fa8f7665309e189
Peter <peter.hsu@concords.com.tw>
2024-10-16 12:55:51 +0800

    ,

---
+++ install_libreoffice.ps1
@@ -0,0 +1,30 @@
+function Install {
+    $latest_version = latest_version
+    # (1) 參數設定 - - - - - - - - - - - - (1) 參數設定 - - - - - - - - - - - - (1) 參數設定 - - - - - - - - - - - -
+    $DownloadUrl = "https://download.documentfoundation.org/libreoffice/portable/24.8.2/LibreOfficePortable_24.8.2_MultilingualStandard.paf.exe"
+    $KiloathDir = Join-Path $HOME "KiloathApp"
+    $Directory = Join-Path $KiloathDir "LibreOffice"
+    $Target = Join-Path $Directory "LibreOfficePortable_24.8.2_MultilingualStandard.paf.exe"
+    $BinDir = "$Directory"
+    # (2) 需要7z來解壓縮 - - - - - - - - - (2) 需要7z來解壓縮 - - - - - - - - - (2) 需要7z來解壓縮 - - - - - - - - -
+    # <#
+    if (-not (Get-Command "7zr.exe" -ErrorAction SilentlyContinue)) {
+        (Invoke-WebRequest "https://raw.githubusercontent.com/kiloath/Installer/main/install_7zr.ps1" -UseBasicParsing).Content | Invoke-Expression
+    }
+
+    New-Item $Directory -Force -ItemType Directory | Out-Null
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest $DownloadUrl -OutFile $Target -UseBasicParsing        
+
+    # (4) 解壓縮 - - - - - - - - - - - - - (4) 解壓縮 - - - - - - - - - - - - - (4) 解壓縮 - - - - - - - - - - - - -
+    Start-Process -FilePath "7zr.exe" -ArgumentList "x $Target -o""$Directory"" -y" | Out-Null
+  
+    # (7) 捷徑 - - - - - - - - - - - - - - (7) 捷徑 - - - - - - - - - - - - - - (7) 捷徑 - - - - - - - - - - - - - -
+    $Shortcut = $WshShell.CreateShortcut("$([Environment]::GetFolderPath('Desktop'))\KiloathApp.lnk")
+    $Shortcut.TargetPath = Join-Path $HOME "KiloathApp"
+    $Shortcut.Save()
+}
+
+Write-Host "--- 安裝 LibreOffice ---"
+Install
+Write-Host "--- 完成 LibreOffice ---"
